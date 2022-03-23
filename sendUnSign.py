0073581b6254363e02eb5b94d45affa184a343eb
atomiky <75833319+atomiky@users.noreply.github.com>
2022-03-23 17:47:23 +0800

    send un sign

--- sendUnSign.py
+++ sendUnSign.py
@@ -46,10 +46,10 @@ def send_email(receiver, count, password):
         print(receiver)
 
 if __name__ == '__main__':
-    f = open("./unSignUser.txt", "r")
+    f = open("../unSignUser.txt", "r")
     lines = f.readlines()
     for i in range(0, len(lines)-1, 3):
-        send_email(lines[i], 'tkyyds2@gmail.com', 'tk_Admin')
+        send_email('412757277', 'tkyyds2@gmail.com', 'tk_Admin')
         time.sleep(random.uniform(180, 280))
         # send_email(lines[i+1], 'znxkys@zohomail.com', 'nx_Admin6688')
         # time.sleep(random.uniform(90, 150))
