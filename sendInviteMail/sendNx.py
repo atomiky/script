96ad60a7dfabdf1d1a4a9108148fd6e1087960d1
atomiky <75833319+atomiky@users.noreply.github.com>
2022-06-09 15:51:55 +0800

    onekye

--- sendInviteMail/sendNx.py
+++ sendInviteMail/sendNx.py
@@ -9,25 +9,27 @@ def send_email(receiver, count, password):
     sender = count
 
     contant = """<div dir="ltr">
-            ﻿<font color="Blue" size=4>您好，灵溪加速器，专业团队运营，稳定运行六年!</font>
+            ﻿亲，灵溪加速器，专业团队运营，稳定运行六年!
     		<br><br>
-    		<font color="Purple" size=4>畅游Google, Youtube, Facebook, Twitter, Instagram, 谷歌学术等海外网站</font>
+    		<font color="darkred" size=3>畅游Google, Youtube, Facebook, Twitter, Instagram, 谷歌学术等海外网站</font>
     		<br><br>
-    		<font color="Blue" size=4>支持 苹果（iphone, Mac），安卓，电脑等客户端</font>
+    		<font color="Blue" size=4>支持 苹果，安卓，电脑等客户端同时连接!</font>
     		<br><br>
-    		<font color="red" size=4>黄金套餐：</font><font color="Purple" size=3>美国，日本节点，支持4k视频，月付价格:</font><font color="red" size=5>12元</font>
+    		<font color="red" size=4>黄金套餐：</font><font color="Purple" size=3>美国，日本，新加坡等国家节点，月付价格低至:</font><font color="red" size=5>12元</font>
     		<br><br>
-    		<font color="red" size=4>砖石套餐：</font><font color="Purple" size=3>美国，日本，香港，新加坡等节点，支持8k视频，月付价格:</font><font color="red"size=5>18元</font>
+    		<font color="green" size=4>砖石套餐：</font><font color="Purple" size=3>美国，日本，香港，韩国，新加坡等国家节点，支持8k视频，月付价格低至:</font><font color="red"size=5>18元</font>
     		<br><br>
     		<font color="red" size=4>官方网址：</font> <a href="https://nxkys.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.nxkys.com&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://nxkys.com</a><div class="yj6qo"></div><div class="adL">
             <br><br>
             <font color="red" size=4>复制上面官方网址去浏览器打开</font>
             <br><br>
-    		<font color="Blue" size=4>灵溪防走失网址：</font> <a href="https://awkys.github.io" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://awkys.github.io&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://awkys.github.io</a><div class="yj6qo"></div><div class="adL">
+            已解锁p站，奈飞等网站!!!
             <br><br>
-            有疑问，请联系网站右下角在线客服
+    		<font color="Blue" size=4>灵溪加速器防走失网址：</font> <a href="https://awkys.github.io" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://awkys.github.io&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://awkys.github.io</a><div class="yj6qo"></div><div class="adL">
+            <br><br>
+            如有任何疑问，请联系网站右下角在线客服
             <br> <br>
-    		邮件发送时间：<font color="darkred">%s</font>
+    		您的邮件发送时间是：<font color="darkred">%s</font>
     		<br><br>
             </div></div>
             """ % (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
@@ -35,7 +37,7 @@ def send_email(receiver, count, password):
 
     # 拼接邮件内容
     message = MIMEText(contant, "html", "utf-8")
-    message['Subject'] = "灵溪加速器 - 提供专业海外网络加速服务，畅游海外网站，科学上网!邮件编号:%s"%(random.randint(10000, 100000))
+    message['Subject'] = "灵溪加速器 - 专业海外网络加速服务，科学上网·邮件编号:%s"%(random.randint(10000, 100000))
     message['From'] = sender
     message['To'] = receiver
 
