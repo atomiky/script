0bc0a7341bf1bec3cff18fe89bb6d70dc1042d0e
atomiky <75833319+atomiky@users.noreply.github.com>
2022-06-13 12:53:33 +0800

    nx

--- expire/sendNxkysExpire.py
+++ expire/sendNxkysExpire.py
@@ -9,27 +9,28 @@ def send_email(receiver, count, password):
     sender = count
 
     contant = """<div dir="ltr">
-        ﻿亲，您好，您在本站的账户<font color="darkred">已经过期</font>!
+        ﻿亲，为不影响您正常使用，请登录网站购买套餐<
         <br><br>
-        为不影响您正常使用，请登录网站购买套餐<<br><br>
+        您在本站的账户<font color="darkred">已经过期</font>!
+        <br><br>
         续费请 <a href="https://nxkys.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://nxkys.com&amp;source=gmail&amp;ust=1638599219093000&amp;usg=AOvVaw1_o00JKGkAtJHfAIdy040a">登录官网</a>，点击<font color="red">'购买套餐'</font> <br> <br>
         续费后请<font color="red">稍等两分钟,</font>然后重新连接客户端 <br> <br>
-        官方网址： <a href="https://nxkys.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://nxkys.com&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://nxkys.com</a><div class="yj6qo"></div><div class="adL"> <br> <br>
-        复制官方地址，去浏览器打开访问
+        灵溪官方网址： <a href="https://nxkys.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://nxkys.com&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://nxkys.com</a><div class="yj6qo"></div><div class="adL"> <br> <br>
+        请复制上面方地址，去浏览器打开访问
         <br> <br>
-        已解锁p站，奈飞等网站!
+        已经解锁p站，奈飞等网站!
         <br> <br>
-        ﻿有疑问，请联系网站右下角在线客服 
+        如有疑问，请联系网站右下角在线客服 
 		<br><br>
 		科学上网，畅游Google, Youtube, Facebook, Twitter, Instagram, 谷歌学术等海外网站。
 		<br><br>
-        本次邮件发送时间为：<font color="darkred">%s</font>
+        发送时间为：<font color="darkred">%s</font>
         """%(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
 
     # 拼接邮件内容
     message = MIMEText(contant, "html", "utf-8")
-    message['Subject'] = "灵溪加速器 - 科学上网账户过期提醒！ 邮件编号:%s"%(random.randint(10000, 100000))
+    message['Subject'] = "灵溪加速器***海外网络加速账户过期提醒！ 邮件编号:%s"%(random.randint(10000, 100000))
     message['From'] = sender
     message['To'] = receiver
 
@@ -48,7 +49,7 @@ if __name__ == '__main__':
     # f = open("../data/unSignUser.txt", "r")
     lines = f.readlines()
     for user in lines:
-        send_email(user, 'yyds@nxbest.men', 'nx_Admin123')
+        send_email(user, 'nxe@nxbest.men', 'nx_Admin123')
         time.sleep(random.uniform(632, 850))
     # for i in range(0, len(lines) - 1, 5):
     #     send_email(lines[i], 'nxkys01@zoho.com.cn', 'fVz3eH6vBj3u')
