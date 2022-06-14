3638f23d7ea997d2c13baba608b8ad449a01e5bd
atomiky <75833319+atomiky@users.noreply.github.com>
2022-06-14 10:12:50 +0800

    nx invite

--- sendInviteMail/sendNx.py
+++ sendInviteMail/sendNx.py
@@ -13,17 +13,21 @@ def send_email(receiver, count, password):
     		<br><br>
     		畅游Google, Youtube, Facebook, Twitter, Instagram, 谷歌学术等海外网站。
     		<br><br>
+    		******************************
     		支持 苹果，安卓，电脑等客户端同时连接!
     		<br><br>
+    		---------------------------------------------------------------------------------
     		黄金套餐：</font><font color="Purple" size=3>美国，日本，新加坡等国家节点，月付价格低至:12元
     		<br><br>
     		砖石套餐：</font><font color="Purple" size=3>美国，日本，香港，韩国，新加坡等国家节点，支持8k视频，月付价格低至:18元
     		<br><br>
     		<font color="red" size=4>官方网址：</font> <a href="https://nxkys.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.nxkys.com&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://nxkys.com</a><div class="yj6qo"></div><div class="adL">
             <br><br>
+            ^^^^^^^^^^^^^^^^^^^^^^
             复制上面官方网址去浏览器打开
             <br><br>
             已解锁p站，奈飞等网站!!!
+            ....................
             <br><br>
     		<font color="Blue" size=4>灵溪加速器防走失网址：</font> <a href="https://awkys.github.io" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://awkys.github.io&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://awkys.github.io</a><div class="yj6qo"></div><div class="adL">
             <br><br>
@@ -37,7 +41,7 @@ def send_email(receiver, count, password):
 
     # 拼接邮件内容
     message = MIMEText(contant, "html", "utf-8")
-    message['Subject'] = "灵溪加速器 - 最稳定的海外网络加速器***邮件编号:%s"%(random.randint(10000, 100000))
+    message['Subject'] = "灵溪加速器 & 最稳定的海外网络加速器--邮件编号:%s"%(random.randint(10000, 100000))
     message['From'] = sender
     message['To'] = receiver
 
