cf09c525184f9b896d36d7e5cec9dd7c97d8ef1a
atomiky <75833319+atomiky@users.noreply.github.com>
2022-06-14 12:44:43 +0800

    onekye

--- expire/sendOnekyExpire.py
+++ expire/sendOnekyExpire.py
@@ -11,21 +11,19 @@ def send_email(receiver, count, password):
     contant = """<div dir="ltr">
         ﻿亲，您在小火箭加速器账户已经过期！
         <br><br>
-        如需继续使用，请登录网站购买套餐后，重新连接即可。<<br><br>
+        ----------
+        如需继续使用，请登录网站续费后，重新连接即可。<<br><br>
 		续费请 <a href="https://www.oneky.xyz" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.oneky.xyz&amp;source=gmail&amp;ust=1638599219093000&amp;usg=AOvVaw1_o00JKGkAtJHfAIdy040a">登录官网</a>，点击<font color="red">'购买套餐'</font> <br> <br>
 		续费后请<font color="red">稍等两分钟,</font>然后重新连接客户端 <br> <br>
 		小火箭加速器-官方网址： <a href="https://www.oneky.xyz" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.oneky.xyz&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://www.oneky.xyz</a><div class="yj6qo"></div><div class="adL"> <br><br>
         请复制上方链接去浏览器打开
         <br> <br>
         如有疑问，请联系网站右下角在线客服！
-        ............................
 		<br><br>
 		已经解锁p站，奈飞等海外网站!!!
 		************************
 		<br><br>
-		浏览Google, Youtube, Facebook, Twitter, Instagram, 谷歌学术等海外网站
-		<br><br>
-		目前支持 苹果，安卓，电脑等客户端同时使用。
+		支持 苹果，安卓，电脑等客户端同时使用。
 		<br><br>
         邮件发送时间：<font color="darkred">%s</font>
         """%(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
@@ -33,7 +31,7 @@ def send_email(receiver, count, password):
 
     # 拼接邮件内容
     message = MIMEText(contant, "html", "utf-8")
-    message['Subject'] = "您的账户过期，请及时续费，邮件编号::%s"%(random.randint(4000, 80000))
+    message['Subject'] = "下火箭加速器账户已过期@邮件编号::%s"%(random.randint(4000, 80000))
     message['From'] = sender
     message['To'] = receiver
 
